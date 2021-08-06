6cc84efcff2f976d5d22c8bbdf3b6da28b39df50
LAPTOP-45RNP5UH\82102 <dev.minseo@gmail.com>
2021-08-06 14:58:20 +0900

    state 사용법, event 관련, bind 함수 사용법

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { render } from '@testing-library/react';
 import React, { Component } from 'react';
 import TOC from "./components/TOC";
 import Content from "./components/content";
@@ -10,19 +9,42 @@ class App extends Component{
   constructor(props) {
     super(props);
     this.state = {
-      subject:{title:'WEB', sub:'World Wide Web!'}
+      mode:'read',
+      subject:{title:'WEB', sub:'World Wide Web!'},
+      welcome:{title:'welcome', desc:'Hello, React!'},
+      contents:[
+        {id:1, title:'HTML', desc:'HTML is for information'},
+        {id:2, title:'CSS', desc:'CSS is for design'},
+        {id:3, title:'JavaScript', desc:'JavaScript is for interactive'}
+      ]
     }
   }
   render() {
+    console.log('App render');
+    var _title, _desc = null;
+    if(this.state.mode === 'welcome') {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if(this.state.mode === 'read') {
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
     return (
       <div className="App">
         <Subject 
           title={this.state.subject.title} 
-          sub={this.state.subject.sub}>
+          sub={this.state.subject.sub}
+          onChangePage={function() {
+            this.setState({mode:'welcome'});
+          }.bind(this)}
+        >
         </Subject>
-        <TOC></TOC>
-        <Content title="HTML" desc="HTML is HyperText Markup 
-          Language"></Content>
+        <TOC 
+          onChangePage={function() {
+            this.setState({mode:'read'});
+          }.bind(this)}
+        data={this.state.contents}></TOC>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     );
   }
